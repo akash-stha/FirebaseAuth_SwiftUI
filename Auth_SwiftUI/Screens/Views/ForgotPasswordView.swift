c5439a4211528f1c57854eb116cc58406efbd157
Akash Shrestha <aakashtha49@gmail.com>
2024-11-16 17:48:53 -0500

    Forgot Password View addeed

---
+++ Auth_SwiftUI/Screens/Views/ForgotPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotPasswordView.swift
+//  Auth_SwiftUI
+//
+//  Created by Akash Shrestha on 2024-11-16.
+//
+
+import SwiftUI
+
+struct ForgotPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgotPasswordView()
+}
