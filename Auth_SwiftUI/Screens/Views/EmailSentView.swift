bc220bdc80377f572a9e43f32847efed64060924
Akash Shrestha <aakashtha49@gmail.com>
2024-11-16 17:59:38 -0500

    Email sent view empty page created

---
+++ Auth_SwiftUI/Screens/Views/EmailSentView.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotPasswordView.swift
+//  Auth_SwiftUI
+//
+//  Created by Akash Shrestha on 2024-11-16.
+//
+
+import SwiftUI
+
+struct EmailSentView: View {
+    var body: some View {
+        Text("Email Sent View")
+    }
+}
+
+#Preview {
+    EmailSentView()
+}
